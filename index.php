a8c8519d9d52547e90b0069f9d6883be5d1f4b53
sellersj <jim.sellers@gmail.com>
2021-11-10 08:00:52 -0500

    printing the php version

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
 echo "Hello World!";
 ?>
 
+<p>PHP version is:
+<?php
+echo phpversion();
+?>
+</p>
+
 <ul>
  <li><a href="subdir/">subdir to test a dir listing</a></li>
  <li><a href="subdir/sub.php">page in the subdir</a></li>
